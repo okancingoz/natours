02fad46556a14ea42ddafdfbcebc8a1a727b2f56
okancingoz <okanc.ngoz@gmail.com>
2025-06-11 09:41:17 +0300

    feat(api): design RESTful endpoints and hange GET/POST requests

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
 
 const app = express();
+
+app.use(express.json());
 
 // app.get('/', (req, res) => {
 //   res
@@ -13,11 +15,40 @@ const app = express();
 //   res.send('You can post to this endpoint...');
 // });
 
-const tours = fs.readFileSync('')
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
 
-app.get('/api/v1/tours',(req,res)=>{
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
 
-})
+app.post('/api/v1/tours', (req, res) => {
+  // console.log(req.body);
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
