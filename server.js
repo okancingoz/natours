6405d9b1ae8052864c65aa95f26b4dc8f2a96628
okancingoz <okanc.ngoz@gmail.com>
2025-06-14 18:15:01 +0300

    feat(config): configure MongoDB connectin and environment variables

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => console.error('Connection error', err));
+
+
+
+  
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
