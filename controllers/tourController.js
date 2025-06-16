22de11b91a0b1b1ca8c1604faaee6f0234e5258b
okancingoz <okanc.ngoz@gmail.com>
2025-06-16 15:25:47 +0300

    feat: add update method to controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -69,6 +69,19 @@ exports.createTour = async (req, res) => {
   }
 };
 
-exports.updateTour = (req, res) => {};
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {}
+};
 
 exports.deleteTour = (req, res) => {};
