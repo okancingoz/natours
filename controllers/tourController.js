2b4ec3a59ea3ad5d5eae38d55ceeb7b95b019fc1
okancingoz <okanc.ngoz@gmail.com>
2025-06-16 15:45:43 +0300

    feat: add delete method to controller

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -64,7 +64,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent',
+      message: err,
     });
   }
 };
@@ -81,7 +81,26 @@ exports.updateTour = async (req, res) => {
         tour,
       },
     });
-  } catch (error) {}
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
-exports.deleteTour = (req, res) => {};
+exports.deleteTour = async (req, res) => {
+  try {
+    await Tour.findByIdAndDelete(req.params.id, {});
+    
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
