c3645fc24c77b63747e15fde91efedadcc820452
okancingoz <okanc.ngoz@gmail.com>
2025-06-17 14:31:40 +0300

    feat(api): add filter method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -12,8 +12,32 @@ exports.checkBody = (req, res, next) => {
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    //BUILD QUERY
+    //FILTERING
+    // eslint-disable-next-line node/no-unsupported-features/es-syntax
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
 
+    // console.log(req.query, queryObj);
+
+    // ADVANCED FILTERING
+    // { difficulty: 'easy', duration: {$gte:5}}
+    //ge,gt,lte,lt
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    //EXECUTE QUERY
+    const tours = await query;
+
+    //SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -92,7 +116,7 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id, {});
-    
+
     res.status(204).json({
       status: 'success',
       data: null,
