c3f3cc5675d480ac9b544709bfad625ce656c3c0
okancingoz <okanc.ngoz@gmail.com>
2025-06-18 15:05:13 +0300

    feat(api): add mmiddleware to alias query

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,6 +10,13 @@ exports.checkBody = (req, res, next) => {
   next();
 };
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,18 +1,21 @@
 const express = require('express');
-const tourController = require("../controllers/tourController");
+const tourController = require('../controllers/tourController');
 
 const router = express.Router();
-
 
 //Create a checkBody middleware
 //Check if body contains the name and price property
 //If not, send back 400 (bad request)
 //Add it to the post handler stack
+router
+  .route('/top-5-cheap')
+  .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
   .route('/')
   .get(tourController.getAllTours)
   .post(tourController.createTour);
+
 router
   .route('/:id')
   .get(tourController.getTour)
