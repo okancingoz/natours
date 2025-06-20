ddae0540b1ac5db3e597be148f97175c61946581
okancingoz <okanc.ngoz@gmail.com>
2025-06-20 16:58:25 +0300

    feat: add error handle method for duplicate db fields

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,7 +1,15 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
 const AppError = require('../utils/appError');
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+  const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
 
@@ -44,9 +52,8 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
 
-    if (error.name === 'CastError') {
-      error = handleCastErrorDB(error);
-    }
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 110000) error = handleDuplicateFieldsDB(error);
 
     sendErrorProd(error, res);
   }
