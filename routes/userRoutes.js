ce5a61f121490736d847af476c88583ef65fdab0
okancingoz <okanc.ngoz@gmail.com>
2025-06-21 15:54:04 +0300

    feat: add simple signup method

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 /* eslint-disable import/no-useless-path-segments */
 const express = require('express');
 const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signup);
+
 router
   .route('/')
   .get(userController.getAllUsers)
