b6daf8327fd7ee674436f2114efc22ebab58b963
okancingoz <okanc.ngoz@gmail.com>
2025-06-28 23:06:37 +0300

    feat: add password reset functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -108,3 +108,19 @@ exports.restrictTo =
 
     next();
   };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // Get user based on POSTed email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  // Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // Send it to user's email
+});
+
+exports.resetPassword = (req, res, next) => {};
