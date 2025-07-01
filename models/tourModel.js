5b274bf3ac85e287c2683a194a8d93a5333883cd
okancingoz <okanc.ngoz@gmail.com>
2025-07-01 12:09:44 +0300

    refactor: reference user docs into tour guides field

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
-const User = require('../models/userModel');
+// const User = require('../models/userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -93,7 +93,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
-    guides: Array,
+    guides: [{ type: mongoose.Schema.ObjectId, ref: 'User' }],
   },
   {
     toJSON: { virtuals: true },
@@ -111,11 +111,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
-tourSchema.pre('save', async function (next) {
-  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// tourSchema.pre('save', async function (next) {
+//   const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
 
 // tourSchema.pre('save', (next) => {
 //   console.log('Will save document...');
