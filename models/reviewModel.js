cbb886c8f0cc8a01c75ddcb29de0b439e6334d16
okancingoz <okanc.ngoz@gmail.com>
2025-07-01 15:59:49 +0300

    feat: add virtual populate for tours and reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -34,7 +34,10 @@ const reviewSchema = new mongoose.Schema(
 );
 
 reviewSchema.pre(/^find/, function (next) {
-  this.populate('tour').populate('user');
+  this.populate({
+    path: 'user',
+    select: 'name photo',
+  });
   next();
 });
 
