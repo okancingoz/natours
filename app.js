3ef9a5d5dd07868e7f5406b6296aba065aa6af40
okancingoz <okanc.ngoz@gmail.com>
2025-07-03 16:35:50 +0300

    feat: creat base layout template usin pug

--- app.js
+++ app.js
@@ -69,7 +69,10 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Okan',
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
