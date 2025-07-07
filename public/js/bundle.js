fdb198c142f40658bb1e48e132fbbe1d5757947a
okancingoz <okanc.ngoz@gmail.com>
2025-07-07 13:42:00 +0300

    feat: image uploads using multer

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12941,7 +12941,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55293" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61330" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
@@ -13086,4 +13086,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/bundle.js.map
+//# sourceMappingURL=bundle.js.map
